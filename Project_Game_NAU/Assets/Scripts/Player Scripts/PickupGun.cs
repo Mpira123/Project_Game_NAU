cefe444c1ea4a844e9f69344ed7778efb1870406
StudentNAU <roma.2017.rozvozchik@gmail.com>
2023-02-19 18:48:40 +0200

    Physic for guns
    Добавив змогу піднімати та викидати зброю, добавив реакцію предметів на попадання зі зброї. Тепер зброя має ефекти та звуки пострілу, а також свою частоту пострілів, зброя тепер має нормальний вигляд(раніше був прямокутний об'єкт)

---
+++ Project_Game_NAU/Assets/Scripts/Player Scripts/PickupGun.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class PickupGun : MonoBehaviour
+{
+    public PlayerGun PlayerGun;
+    public Rigidbody rb;
+    public BoxCollider coll;
+    public Transform player, gunContainer, fpsCam;
+
+    public float pickUpRange;
+    public float dropForwardForce, dropUpwardForce;
+
+    public bool equipped;
+    public static bool slotFull;
+
+    private void Start()
+    {
+        if (!equipped)
+        {
+            PlayerGun.enabled = false;
+            rb.isKinematic = false;
+            coll.isTrigger = false;
+        }
+        if (equipped)
+        {
+            PlayerGun.enabled = true;
+            rb.isKinematic = true;
+            coll.isTrigger = true;
+            slotFull = true;
+        }
+    }
+    private void Update()
+    {
+        Vector3 distaceToPlayer = player.position - transform.position;
+        if (!equipped && distaceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.E) && !slotFull) PickUp();
+
+        if (equipped && Input.GetKeyDown(KeyCode.Q)) Drop();
+    }
+
+    private void PickUp()
+    {
+        equipped = true;
+        slotFull = true;
+
+        transform.SetParent(gunContainer);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localScale = Vector3.one;
+
+        rb.isKinematic = true;
+        coll.isTrigger = true;
+
+        PlayerGun.enabled = true;
+    }
+
+    private void Drop()
+    {
+        equipped = false;
+        slotFull = false;
+
+        transform.SetParent(null);
+
+        rb.isKinematic = false;
+        coll.isTrigger = false;
+
+        rb.velocity = player.GetComponent<Rigidbody>().velocity;
+
+        rb.AddForce(fpsCam.forward * dropForwardForce, ForceMode.Impulse);
+        rb.AddForce(fpsCam.up * dropUpwardForce, ForceMode.Impulse);
+
+        float random = Random.Range(-1f, 1f);
+        rb.AddTorque(new Vector3(random, random, random) * 10);
+
+        PlayerGun.enabled = false;
+    }
+}
+
--- Project_Game_NAU/Assets/Scripts/Player Scripts/PlayerGun.cs
+++ Project_Game_NAU/Assets/Scripts/Player Scripts/PlayerGun.cs
@@ -4,23 +4,39 @@ public class PlayerGun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 10f;
+    private float NextFire = 0f;
+    public float force = 100f;
     public Camera fpsCam;
+    public Transform bulletSpawner;
+    public ParticleSystem muzzleflash;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
    
    
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))    
+        if(Input.GetKey(KeyCode.Mouse0) && Time.time > NextFire)    
         {
+            NextFire = Time.time + 1f / fireRate;
             Shoot();
         }
 
         void Shoot()
-        { 
+        {
+            audioSource.PlayOneShot(audioClip);
+            //Instantiate(muzzleflash, bulletSpawner.position,bulletSpawner.rotation);
+            muzzleflash.Play();
+
             RaycastHit hit;
 
             if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) 
             {
                 Debug.Log(hit.transform.name);
+                if(hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(-hit.normal * force);
+                }
             }
 
            Target target = hit.transform.GetComponent<Target>();
